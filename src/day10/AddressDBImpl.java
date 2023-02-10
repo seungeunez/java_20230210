6da969c92488cf228078d9646b24dc8221733ff4
탁승은 <xkrtmddms@gmail.com>
2023-02-10 11:36:12 +0900

    DB
    map 사용

--- src/day10/AddressDBImpl.java
+++ src/day10/AddressDBImpl.java
@@ -1,13 +1,16 @@
 package day10;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.bson.Document;
 
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.InsertOneResult;
 
 public class AddressDBImpl implements AddressDB {
 
@@ -32,7 +35,31 @@ public class AddressDBImpl implements AddressDB {
 	@Override
 	public int insertAddress(Address address) {
 
-		return 0;
+		try {
+
+			// 시퀀스를 이용해서 숫자 가져오기, 얘도 반복작업 수준이라 따로 만들어서 소환시키는게 낫다
+			Document doc = this.sequence.findOneAndUpdate(Filters.eq("_id", "SEQ_ADDRESS_CODE"), Updates.inc("idx", 1));
+			long code = doc.getLong("idx"); // = (long)doc.get("idx");
+
+			Document saveDoc = new Document();
+			saveDoc.append("_id", code);
+			saveDoc.append("address", address.getAddress());
+			saveDoc.append("postcode", address.getPostcode());
+			saveDoc.append("regdate", address.getRegdate());
+			saveDoc.append("memberid", address.getMemberid().getId());
+
+			// ****회원정보 전체 추가하지 않음, 기본키인 아이디만 추가함*****
+
+			InsertOneResult result = this.addresses.insertOne(saveDoc);
+			if (result.getInsertedId().asInt64().getValue() == code) {
+				return 1;
+			}
+			return 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+
 	}
 
 //===============================================================================
@@ -40,18 +67,16 @@ public class AddressDBImpl implements AddressDB {
 	// map 이용 - 등록
 	@Override
 	public int insertAddressMap(Map<String, Object> map) {
-		try {
-			
-			map.get("_id");
 
-			
-			
+		try {
+
+			return 0;
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return -1;
 		}
 
-		return 0;
 	}
 
 //===============================================================================
--- src/day10/AddressMain.java
+++ src/day10/AddressMain.java
@@ -3,7 +3,11 @@ package day10;
 public class AddressMain {
 
 	public static void main(String[] args) {
-
+		
+		
+		
+		
+		
 	}
 
 }
